44c3bb9389170544922c55d8450ff57d848df59e
rubenlamoot <ruben.lamoot1@pandora.be>
2019-06-24 11:52:36 +0200

    final commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,80 +1,3 @@
-{{--@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection--}}
 
 @extends('layouts.master')
 
@@ -215,21 +138,21 @@
                     <div class="carousel-inner">
                         <div class="carousel-item active">
                             <div>
-                                <img src="assets/images/getuige1.jpg" class="d-block float-left pr-3" width="135" height="135" alt="getuige1">
+                                <img src="{{asset('images/home/getuige1.jpg')}}" class="d-block float-left pr-3" width="135" height="135" alt="getuige1">
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci cupiditate inventore neque pariatur quis quod veniam voluptate! Aperiam enim iusto nam nesciunt voluptas! Atque deleniti maxime nobis quaerat quasi totam.</p>
                             </div>
                             <p><span class="font-weight-bold">Roberta Curabitus</span>, Zwolle Nederland</p>
                         </div>
                         <div class="carousel-item">
                             <div>
-                                <img src="assets/images/getuige2.jpg" class="d-block float-left pr-3" width="135" height="135" alt="getuige2">
+                                <img src="{{asset('images/home/getuige2.jpg')}}" class="d-block float-left pr-3" width="135" height="135" alt="getuige2">
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci cupiditate inventore neque pariatur quis quod veniam voluptate! Aperiam enim iusto nam nesciunt voluptas! Atque deleniti maxime nobis quaerat quasi totam.</p>
                             </div>
                             <p><span class="font-weight-bold">Lauren Demets</span>, Brugge België</p>
                         </div>
                         <div class="carousel-item">
                             <div>
-                                <img src="assets/images/getuige3.jpg" class="d-block float-left pr-3" width="135" height="135" alt="getuige3">
+                                <img src="{{asset('images/home/getuige3.jpg')}}" class="d-block float-left pr-3" width="135" height="135" alt="getuige3">
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci cupiditate inventore neque pariatur quis quod veniam voluptate! Aperiam enim iusto nam nesciunt voluptas! Atque deleniti maxime nobis quaerat quasi totam.</p>
                             </div>
                             <p><span class="font-weight-bold">Katrien Daneels</span>, Ieper België</p>
